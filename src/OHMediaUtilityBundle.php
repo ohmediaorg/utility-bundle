7b7fef16bee5a36e0a0bd3ca7982bd5ee3c35a71
justin-oh <justin@ohmedia.ca>
2023-12-08 14:34:27 -0600

    deprecation fix

--- src/OHMediaUtilityBundle.php
+++ src/OHMediaUtilityBundle.php
@@ -7,8 +7,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OHMediaUtilityBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }
 }
