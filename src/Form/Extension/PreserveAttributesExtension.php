096105371d0a336c683ddd04fc3d0f6e8e6d3bd3
sean <seanpeterson.997@gmail.com>
2025-06-24 12:06:55 -0600

    Set Regex

--- src/Form/Extension/PreserveAttributesExtension.php
+++ src/Form/Extension/PreserveAttributesExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PreserveAttributesExtension extends AbstractTypeExtension
@@ -15,10 +16,40 @@ class PreserveAttributesExtension extends AbstractTypeExtension
     {
     }
 
+    // TODO types
+    private function setMaxlength($metadata, $property, FormView $view): void
+    {
+        foreach ($metadata->getPropertyMetadata($property) as $propMeta) {
+            foreach ($propMeta->getConstraints() as $constraint) {
+                if ($constraint instanceof Length && $constraint->max) {
+                    $view->vars['attr']['maxlength'] ??= $constraint->max;
+                    return;
+                }
+            }
+        }
+    }
+
+    private function setPattern($metadata, $property, FormView $view): void
+    {
+        // TODO DRY
+        foreach ($metadata->getPropertyMetadata($property) as $propMeta) {
+            foreach ($propMeta->getConstraints() as $constraint) {
+                if ($constraint instanceof Regex && $constraint->pattern) {
+                    $view->vars['attr']['pattern'] ??= $constraint->pattern;
+                    return;
+                }
+            }
+        }
+    }
+
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
         $dataClass = $form->getParent()->getConfig()->getOption('data_class');
         $property = $form->getName();
+
+        // TODO
+        $innerType = $form->getConfig()->getType()->getInnerType();
+        $type = $form->getConfig()->getType();
 
         if (!$dataClass || !class_exists($dataClass)) {
             return;
@@ -29,23 +60,8 @@ class PreserveAttributesExtension extends AbstractTypeExtension
             return;
         }
 
-        $test = $metadata->getPropertyMetadata($property);
-
-        $maxlength = null;
-        foreach ($metadata->getPropertyMetadata($property) as $propMeta) {
-            $testTwo = $propMeta->getConstraints();
-
-            foreach ($propMeta->getConstraints() as $constraint) {
-                if ($constraint instanceof Length && $constraint->max) {
-                    $maxlength = $constraint->max;
-                    break 2;
-                }
-            }
-        }
-
-        if ($maxlength) {
-            $view->vars['attr']['maxlength'] ??= $maxlength;
-        }
+        $this->setMaxlength($metadata, $property, $view);
+        $this->setPattern($metadata, $property, $view);
     }
 
     public static function getExtendedTypes(): iterable
