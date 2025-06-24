697562fca2e26a233bbab97ce86d2eaac9d7319d
sean <seanpeterson.997@gmail.com>
2025-06-24 12:23:04 -0600

    PreserveAttributesExtension - refactor to use ValidatorTypeGuesser

--- src/Form/Extension/PreserveAttributesExtension.php
+++ src/Form/Extension/PreserveAttributesExtension.php
@@ -4,41 +4,30 @@ namespace OHMedia\UtilityBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Validator\ValidatorTypeGuesser;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PreserveAttributesExtension extends AbstractTypeExtension
 {
-    public function __construct(private ValidatorInterface $validator)
+    public function __construct(private ValidatorTypeGuesser $guesser)
     {
     }
 
     // TODO types
-    private function setMaxlength($metadata, $property, FormView $view): void
+    private function setMaxlength($dataClass, $property, $view): void
     {
-        foreach ($metadata->getPropertyMetadata($property) as $propMeta) {
-            foreach ($propMeta->getConstraints() as $constraint) {
-                if ($constraint instanceof Length && $constraint->max) {
-                    $view->vars['attr']['maxlength'] ??= $constraint->max;
-                    return;
-                }
-            }
+        $maxlengthGuess = $this->guesser->guessMaxLength($dataClass, $property);
+        if ($maxlengthGuess && $maxlengthGuess->getValue()) {
+            $view->vars['attr']['maxlength'] ??= $maxlengthGuess->getValue();
         }
     }
 
-    private function setPattern($metadata, $property, FormView $view): void
+    private function setPattern($dataClass, $property, $view): void
     {
-        // TODO DRY
-        foreach ($metadata->getPropertyMetadata($property) as $propMeta) {
-            foreach ($propMeta->getConstraints() as $constraint) {
-                if ($constraint instanceof Regex && $constraint->pattern) {
-                    $view->vars['attr']['pattern'] ??= $constraint->pattern;
-                    return;
-                }
-            }
+        $patternGuess = $this->guesser->guessPattern($dataClass, $property);
+        if ($patternGuess && $patternGuess->getValue()) {
+            $view->vars['attr']['pattern'] ??= $patternGuess->getValue();
         }
     }
 
@@ -55,13 +44,8 @@ class PreserveAttributesExtension extends AbstractTypeExtension
             return;
         }
 
-        $metadata = $this->validator->getMetadataFor($dataClass);
-        if (!method_exists($metadata, 'getPropertyMetadata')) {
-            return;
-        }
-
-        $this->setMaxlength($metadata, $property, $view);
-        $this->setPattern($metadata, $property, $view);
+        $this->setMaxlength($dataClass, $property, $view);
+        $this->setPattern($dataClass, $property, $view);
     }
 
     public static function getExtendedTypes(): iterable
