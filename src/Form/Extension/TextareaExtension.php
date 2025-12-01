846d59c9b95be5565db9803a854395a0e6420ec2
Justin Hoffman <justin@ohmedia.ca>
2025-12-01 17:08:55 -0600

    remove carriage returns from textarea values

---
+++ src/Form/Extension/TextareaExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OHMedia\UtilityBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TextareaExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $removeCarriageReturn = function (?string $value): string {
+            if ($value) {
+                $value = str_replace(["\r\n", "\r"], "\n", $value);
+            }
+
+            return $value;
+        };
+
+        $builder->addModelTransformer(new CallbackTransformer(
+            $removeCarriageReturn,
+            $removeCarriageReturn,
+        ));
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [TextareaType::class];
+    }
+}
