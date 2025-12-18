e5f5698f1eea93f3f67c7c3cdb76b4f597beddf9
Justin Hoffman <justin@ohmedia.ca>
2025-12-18 16:49:28 -0600

    add CallToAction entity

---
+++ src/Entity/CallToAction.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use OHMedia\UtilityBundle\Repository\CallToActionRepository;
+
+#[ORM\Entity(repositoryClass: CallToActionRepository::class)]
+class CallToAction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 8)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $entity = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $url = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $text = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $new_window = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getEntity(): ?string
+    {
+        return $this->entity;
+    }
+
+    public function setEntity(?string $entity): static
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function isNewWindow(): ?bool
+    {
+        return $this->new_window;
+    }
+
+    public function setNewWindow(?bool $new_window): static
+    {
+        $this->new_window = $new_window;
+
+        return $this;
+    }
+}
