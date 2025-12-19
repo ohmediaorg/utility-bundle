beec69cd879dc1cfa29ef94df767411ccb1ac023
Justin Hoffman <justin@ohmedia.ca>
2025-12-19 10:57:59 -0600

    preset form field visibility in php using form event data

--- src/Form/CallToActionType.php
+++ src/Form/CallToActionType.php
@@ -5,10 +5,12 @@ namespace OHMedia\UtilityBundle\Form;
 use OHMedia\UtilityBundle\Entity\CallToAction;
 use OHMedia\UtilityBundle\Service\EntityPathManager;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PreSetDataEvent;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,33 +24,48 @@ class CallToActionType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $entityChoices = $this->entityPathManager->getChoices();
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (PreSetDataEvent $event) {
+            $callToAction = $event->getData();
+            $form = $event->getForm();
 
-        if ($entityChoices) {
-            $builder->add('type', ChoiceType::class, [
-                'label' => 'Link Type',
-                'choices' => [
-                    'Internal' => CallToAction::TYPE_INTERNAL,
-                    'External' => CallToAction::TYPE_EXTERNAL,
+            $entityChoices = $this->entityPathManager->getChoices();
+
+            if ($entityChoices) {
+                $form->add('type', ChoiceType::class, [
+                    'label' => 'Link Type',
+                    'choices' => [
+                        'Internal' => CallToAction::TYPE_INTERNAL,
+                        'External' => CallToAction::TYPE_EXTERNAL,
+                    ],
+                ]);
+
+                $form->add('entity', ChoiceType::class, [
+                    'label' => 'Internal Resource',
+                    'choices' => $entityChoices,
+                    'row_attr' => [
+                        'style' => $callToAction->isTypeInternal() ? '' : 'none',
+                    ],
+                ]);
+
+                $showUrl = $callToAction->isTypeExternal();
+            } else {
+                $form->add('type', HiddenType::class, [
+                    'data' => CallToAction::TYPE_EXTERNAL,
+                ]);
+
+                $showUrl = true;
+            }
+
+            $form->add('url', UrlType::class, [
+                'label' => 'External URL',
+                'default_protocol' => null,
+                'row_attr' => [
+                    'style' => $showUrl ? '' : 'none',
                 ],
             ]);
 
-            $builder->add('entity', ChoiceType::class, [
-                'label' => 'Internal Resource',
-                'choices' => $entityChoices,
-            ]);
-        } else {
-            $builder->add('type', HiddenType::class, [
-                'data' => CallToAction::TYPE_EXTERNAL,
-            ]);
-        }
-
-        $builder->add('url', UrlType::class, [
-            'label' => 'External URL',
-            'default_protocol' => null,
-        ]);
-
-        $builder->add('text');
+            $form->add('text');
+        });
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options): void
