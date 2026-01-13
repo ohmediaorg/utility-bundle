b5a7dbaac692e37ca19db02a7c57bd4472cb4c0b
Justin Hoffman <justin@ohmedia.ca>
2026-01-13 16:36:38 -0600

    ditch alias and fix service argument

--- src/DependencyInjection/Compiler/EntityPathPass.php
+++ src/DependencyInjection/Compiler/EntityPathPass.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\UtilityBundle\DependencyInjection\Compiler;
 
+use OHMedia\UtilityBundle\Service\EntityPathManager;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -11,11 +12,11 @@ class EntityPathPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         // always first check if the primary service is defined
-        if (!$container->has('oh_media_utility.entity_path_manager')) {
+        if (!$container->has(EntityPathManager::class)) {
             return;
         }
 
-        $definition = $container->findDefinition('oh_media_utility.entity_path_manager');
+        $definition = $container->findDefinition(EntityPathManager::class);
 
         $tagged = $container->findTaggedServiceIds('oh_media_utility.entity_path_provider');
 
