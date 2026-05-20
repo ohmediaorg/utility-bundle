bcfb07530b59f3d8e9d386c84af19d651e573bae
Justin Hoffman <justin@ohmedia.ca>
2026-05-20 10:35:16 -0600

    deprecation fix

--- src/Validator/PostalCode.php
+++ src/Validator/PostalCode.php
@@ -2,25 +2,22 @@
 
 namespace OHMedia\UtilityBundle\Validator;
 
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\RegexValidator;
 
 #[\Attribute]
 class PostalCode extends Regex
 {
+    #[HasNamedArguments]
     public function __construct(
         string|array|null $pattern = null,
         ?string $message = null,
-        ?string $htmlPattern = null,
-        ?bool $match = null,
-        ?callable $normalizer = null,
-        ?array $groups = null,
-        mixed $payload = null,
-        array $options = []
     ) {
-        $options['pattern'] = $options['value'] = '/^[A-Za-z]\d[A-Za-z]\s?\d[A-Za-z]\d$/';
-
-        parent::__construct($options, $groups, $payload);
+        parent::__construct(
+            pattern: '/^[A-Za-z]\d[A-Za-z]\s?\d[A-Za-z]\d$/',
+            message: $message,
+        );
     }
 
     public function validatedBy(): string
